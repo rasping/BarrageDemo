8090bfd9cc30b772a2d681376f0e5c2a1a163731
rasping <rasping@163.com>
2016-09-27 08:59:18 +0800

    IOS弹幕实现
    初始版本

---
+++ BarrageDemo/BarrageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BarrageDemo
+//
+//  Created by siping ruan on 16/9/8.
+//  Copyright © 2016年 siping ruan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BarrageDemo/BarrageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BarrageDemo
+//
+//  Created by siping ruan on 16/9/8.
+//  Copyright © 2016年 siping ruan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
